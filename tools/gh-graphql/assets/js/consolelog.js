b1741f0953ad379841ba978a63c1a4885d9ee6fe
jxmot <webdev@stupiddog.net>
2020-12-17 01:05:07 -0600

    comments

--- tools/gh-graphql/assets/js/consolelog.js
+++ tools/gh-graphql/assets/js/consolelog.js
@@ -1,7 +1,12 @@
 // A console.log() replacement, a boolean variable
 // is used for enabling or disabling its output.
+
+// this variable name uses the underscore in its 
+// name to avoid potential conflicts with other
+// code that might use 'debug' for its own purposes.
 const _debug = true;
 
+// nothing fancy, just the basics...
 function consolelog(msg) {
     if (_debug === true) {
         console.log(msg);
